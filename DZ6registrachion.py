008aed16bc935943a1bf849d402b64fbed725f26
admin <admin@MacBook-admin.local>
2022-03-29 14:37:09 +0300

    Ispravila registracion

--- DZ6registrachion.py
+++ DZ6registrachion.py
@@ -1,33 +1,48 @@
 
-# def decorator_regisr(func):
-#     def wrapper():
-#         func() 
-#     return wrapper
+def decorator_regisr(func):
+    def wrapper(user, password):
+        if len(password) < 8:
+            print("Incorrect password lengh")
+        else: 
+            print ('The password lenght is correct')
+        simv = ". ? , ! * - + /"
+        for simbol in simv:
+            if simbol in password:
+                print("The special symbol in the password")
+            else: pass
+        for letter in password:
+            if letter.isupper():
+                print("Dont use upper letters")
+        func(user, password) 
+    return wrapper
 
 
-new_file = open ('users.txt', "w")
-# @decorator_regisr
-def registration ():
-    user = input("Login:")
-    password = input("Password:")
+new_file = open('users.txt', "w")
+@decorator_regisr
+def registration (user, password):
     new_file.write(f'{user}:{password}''\n')
     return 
+
+user = input("Login:")
+password = input("Password:")
     
 
-registration()
-new_file.close
+registration(user, password)
+new_file.close()
 
 
 new_file = open ("users.txt", "r")
 read_users= new_file.read()
 
 def autorization():
-    login = input("enter your login:")
-    passw = input("enter your password:")
-    if login in read_users and passw in read_users:
+    login = input("Enter your login:")
+    passw = input("Enter your password:")
+    x = f'{login}:{passw}'
+    print(x)
+    if x in read_users:
         print("Successfully")
     else:
         print("Incorrect login or password")
 autorization()
 
-new_file.close
+new_file.close()
