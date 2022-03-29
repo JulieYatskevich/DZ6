f74de43643d2e30bc10d36e4ba54bed8c3e410b6
admin <admin@MacBook-admin.local>
2022-03-29 13:17:22 +0300

    correct Dec and kambda

--- DZ6decorator.py
+++ DZ6decorator.py
@@ -1,4 +1,4 @@
-def decorator1(func):
+def privetsntvie(func):
     """Декоратор выдает приветствие и указывает в какую группу определен человек"""
     def wrapper():
         name = str(input("Enter you Name:"))
@@ -12,7 +12,7 @@ def decorator1(func):
 def sort_lastname():
     """Функция сортирующая фамилии на группы согласно алфавитного порядка"""
     lastname = str(input("Enter you Lastname:"))
-    if (lastname <="K" ):
+    if (lastname <= "K"):
         print("Your group is number 1")
     elif (lastname > "K" and lastname <= "S"):
         print("Your group is number 2")
--- DZ6lambda.py
+++ DZ6lambda.py
@@ -1,13 +1,16 @@
-even_list = [1,3,5,7,9,11,13]
-odd_list = [2,4,6,8,10,12,14]
+from functools import reduce
 
-sum_list = list(map(lambda x,y: x*y, even_list, odd_list))
+
+even_list = [1, 3, 5, 7, 9, 11, 13]
+odd_list = [2, 4, 6, 8, 10, 12, 14]
+
+sum_list = list(map(lambda x, y: x * y, even_list, odd_list))
 print(sum_list)
 
-fil_numbers = list(filter(lambda x: x >= 56, sum_list))
+filtered_numbers = list(filter(lambda x: x >= 56, sum_list))
 print(fil_numbers)
 
 
-from functools import reduce
-sum_numbers = reduce(lambda x,y: x+y, fil_numbers)
+
+sum_numbers = reduce(lambda x, y: x + y, fil_numbers)
 print(sum_numbers)
